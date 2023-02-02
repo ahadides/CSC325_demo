47de877a5c0004439d07dfbc67578b7ba04608e1
ahmad <ahmad@DESKTOP-5CT1GNH>
2023-02-02 10:02:06 -0500

    Worked on Creating a class

---
+++ src/main/java/com/mycompany/pjrdemo/TestClass.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pjrdemo;
+
+/**
+ *
+ * @author ahmad
+ */
+public class TestClass {
+    private String TC;
+
+    public TestClass(String TC) {
+        this.TC = TC;
+    }
+
+    public String getTC() {
+        return TC;
+    }
+
+    public void setTC(String TC) {
+        this.TC = TC;
+    }
+    
+}
